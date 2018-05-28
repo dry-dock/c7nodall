0fde8ffaa02df6a7296d9460784bd947189f3314
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 17:56:17 +0530

    Update 10_2_1.sh

--- version/10_2_1.sh
+++ version/10_2_1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 NODE_VERSION=10.2.1
-NPM_VERSION=5.6.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
-nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
+nvm use $NODE_VERSION
