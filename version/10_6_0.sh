a5f0d028c92bcdf22d3b92ee51500640f8d0a571
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-08-17 11:43:53 +0530

    Update 10_6_0.sh

--- version/10_6_0.sh
+++ version/10_6_0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-NODE_VERSION=10.6.0
+NODE_VERSION=10.8.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 nvm use $NODE_VERSION
