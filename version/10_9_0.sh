e5871ded47d164c7df83becc6f7a45f5a1ff229d
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-08-27 17:39:09 +0530

    Update 10_9_0.sh

--- version/10_9_0.sh
+++ version/10_9_0.sh
@@ -6,4 +6,3 @@ NPM_VERSION=6.4.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
-
